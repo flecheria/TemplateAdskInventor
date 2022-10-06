95b799ef5d90814a3e34eec316489374c8a27a15
Paolo Cappelletto a.k.a. flecheria <info@flecheria.it>
2022-10-06 16:09:29 +0200

    updat assembly info

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("$guid1$")]
+[assembly: Guid("$guid2$")]
 
 //
 // Version information for an assembly consists of the following four values:
